98441c3122eab1cc0bf223cca75e413c644cd5b2
haroo <haroon.nasiri2023@gmail.com>
2024-02-01 17:03:22 -0600

    first_week_java_questions

---
+++ src/haroon/EvenAndOdd.java
@@ -0,0 +1,30 @@
+package haroon;
+
+public class EvenAndOdd {
+    public static void main(String[] args) {
+
+        int number = 6;
+        boolean isEven = number % 2 == 0;
+        boolean isOdd = !isEven;
+
+        System.out.println(number + " is an even number " + isEven);
+        System.out.println(number + " is an odd number " + isOdd);
+
+        System.out.println("-----------------------------------------------------------------------");
+
+        int number1 = 5; // You can change the number to test different values
+
+        if (number1 % 2 == 0) {
+            System.out.println(number1 + " is an even number");
+        } else {
+            System.out.println(number1 + " is an odd number");
+
+
+
+
+
+        }
+
+    }
+
+}
