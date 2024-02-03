94fd1758102e4171361cdb7cb7296395bf7ee8b7
melikecorasag <melikecora12@gmail.com>
2024-02-03 11:36:31 -0600

    Create Week02_Q01,Q02,Q03 class - Swap two numbers- Divisible 15,3,5 - ConsecutiveNumbers

---
+++ src/melike/Week02_Q01.java
@@ -0,0 +1,27 @@
+package melike;
+
+public class Week02_Q01 {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 5;
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+
+
+
+    }
+}
+/*
+Swap two variables' values without using a third variable
+ */
+
+
+
