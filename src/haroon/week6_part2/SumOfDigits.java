f73565697c30fe22585a12ee476347e822fa6794
haroon <haroon.nasiri2023@gmail.com>
2024-03-11 09:20:42 -0500

    add 6 weeks tasks

--- src/haroon/week6_part2/SumOfDigits.java
+++ src/haroon/week6_part2/SumOfDigits.java
@@ -5,19 +5,31 @@ import com.sun.tools.javac.Main;
 public class SumOfDigits {
     public static void main(String[] args) {
         String str = "12 java 5 apple 3";
-        System.out.println(sumOfDigits(str));
+        System.out.println(sumOfDigits(str)); // Output: 20
     }
 
-    public static int sumOfDigits(String input){
-
+    public static int sumOfDigits(String input) {
         int sum = 0;
+        StringBuilder numString = new StringBuilder();
+
         for (int i = 0; i < input.length(); i++) {
             char currentChar = input.charAt(i);
-            if (Character.isDigit(currentChar)){
-                sum += Character.getNumericValue(currentChar);
-            }
 
+            if (Character.isDigit(currentChar)) {
+                numString.append(currentChar);
+            } else {
+                if (numString.length() > 0) {
+                    sum += Integer.parseInt(numString.toString());
+                    numString.setLength(0);
+                }
+            }
         }
+
+
+        if (numString.length() > 0) {
+            sum += Integer.parseInt(numString.toString());
+        }
+
         return sum;
     }
 
